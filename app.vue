43f70804208ca8d68a4a78460848c572b0e3a57e
Jan Kaderabek <kaderabek.jan@gmail.com>
2025-04-28 14:41:16 +0200

    feat: simple images fetch

--- app.vue
+++ app.vue
@@ -1,6 +1,11 @@
+<script lang="ts" setup>
+  const { data: images } = await useFetch('/api/images');
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div v-if="images">
+    <div v-for="image in images" :key="image.pathname">
+      <img :src="`/images/${image.pathname}`" :alt="image.pathname" width="400px" />
+    </div>
   </div>
 </template>
