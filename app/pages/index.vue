7b9ecbc68bb94ef8c1cc9cf73ea23a6df536a92e
Jan Kaderabek <kaderabek.jan@gmail.com>
2025-05-12 15:53:17 +0200

    remove your albums section

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -27,50 +27,5 @@ const heroLinks = computed(() => {
       description="Store and organize your photos in albums"
       :links="heroLinks"
     />
-
-    <UPageSection title="Your Albums">
-      <UAlert
-        v-if="!albums || albums.length === 0"
-        icon="i-heroicons-information-circle"
-        color="neutral"
-        title="No albums yet"
-        description="Create your first album to get started."
-      />
-
-      <UCard
-        v-else
-        class="mb-4"
-      >
-        <UPageGrid cols="1:3">
-          <div
-            v-for="album in albums.slice(0, 3)"
-            :key="album.name"
-            class="cursor-pointer"
-          >
-            <NuxtLink
-              :to="`/albums/${album.name}`"
-              class="block"
-            >
-              <UCard class="hover:shadow-lg transition-shadow">
-                <template #header>
-                  <h3 class="text-lg font-medium">{{ album.name }}</h3>
-                </template>
-              </UCard>
-            </NuxtLink>
-          </div>
-        </UPageGrid>
-
-        <template #footer>
-          <UButton
-            to="/albums"
-            color="neutral"
-            variant="ghost"
-            trailing-icon="i-heroicons-arrow-right"
-          >
-            View all albums
-          </UButton>
-        </template>
-      </UCard>
-    </UPageSection>
   </UContainer>
 </template>
